2752c7eb4dd8f9f7a0a843a2b356a7a33112a27f
Donghyeon Cho <41141851+hyeon9698@users.noreply.github.com>
2020-07-13 22:49:30 +0900

    Create 탑.cpp

---
+++ programmers/level2/탑.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> heights) {
+    vector<int> answer;
+    vector<pair<int,int>> v;
+    int count=0;
+    for(int i=0;i<heights.size();i++){
+        v.push_back(make_pair(i+1,heights[i]));
+    }
+    for(int i=0;i<heights.size();i++){
+        count=0;
+        for(int j=i;j>=0;j--){
+            if(heights[i]<heights[j]){
+                answer.push_back(v[j].first);
+                count=1;
+                break;
+            }
+        }
+        if(count==0)answer.push_back(0);
+    }
+    return answer;
+}
