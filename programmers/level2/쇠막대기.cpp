bb20c9b466d4bf84720abb21eb971f2f3b8e5afc
Donghyeon Cho <41141851+hyeon9698@users.noreply.github.com>
2020-07-13 22:24:42 +0900

    Create 쇠막대기.cpp

---
+++ programmers/level2/쇠막대기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string arrangement) {
+    int answer = 0;
+    vector<int> v;    
+    for(int i=0;i<arrangement.length();i++){
+        if(arrangement[i]=='(' && arrangement[i+1]==')'){
+            for(int j=0;j<v.size();j++){
+                v[j]++;
+            }
+            i++;
+        }
+        else if(arrangement[i]==')'){
+            answer += v.back() + 1;
+            v.pop_back();
+        }
+        else{
+            v.push_back(0);
+        }
+    }
+    return answer;
+}
