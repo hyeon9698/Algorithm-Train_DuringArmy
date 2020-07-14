32e630fefefb13ffdb297c6e3e010a735fbe995a
Donghyeon Cho <41141851+hyeon9698@users.noreply.github.com>
2020-07-14 23:54:04 +0900

    Create 숫자의 표현.cpp

---
+++ programmers/level2/숫자의 표현.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int total = 0;
+    for(int i=1;i<=n;i++){
+        total = 0;
+        for(int j=i;j<=n;j++){
+            if(total > n){
+                break;
+            }            
+            total = total + j;
+            if(total == n){
+                answer++;
+            }
+        }
+    }
+    return answer;
+}
