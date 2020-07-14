3c090a6b72ba3488a6d4a039b37ca4e8aa2c5db3
Donghyeon Cho <41141851+hyeon9698@users.noreply.github.com>
2020-07-14 23:23:11 +0900

    Update JadenCase 문자열 만들기.cpp

--- programmers/level2/JadenCase 문자열 만들기.cpp
+++ programmers/level2/JadenCase 문자열 만들기.cpp
@@ -1,24 +1,14 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 string solution(string s) {
     string answer = "";
-    int sol = 0;
-    for(int i = 0;i<s.size();i++){
-        if(s[i-1]==' ' && s[i]!=' '){
-            if(s[i]>='a'&&s[i]<='z'){
-                s[i] = s[i] + ('A'-'a');
-            }
-        }
-        else if(s[i]>='A'&&s[i]<='Z'){
-            s[i] = s[i] - ('A'-'a');
-        }
-        else if(s[0] >='a'&&s[0]<='z'){
-            s[0] = s[0] + ('A'-'a');
-        }
-    }
-    return s;
+
+    answer += toupper(s[0]);
+    for (int i = 1; i < s.size(); i++)
+        s[i - 1] == ' ' ? answer += toupper(s[i]) : answer += tolower(s[i]);
+
+    return answer;
 }
