f3eef98837d06463d81c138e38ccd0b53e109763
Donghyeon Cho <41141851+hyeon9698@users.noreply.github.com>
2020-07-16 00:01:02 +0900

    Update JadenCase 문자열 만들기.cpp

--- programmers/level2/JadenCase 문자열 만들기.cpp
+++ programmers/level2/JadenCase 문자열 만들기.cpp
@@ -6,6 +6,7 @@ using namespace std;
 string solution(string s) {
     string answer = "";
     answer += toupper(s[0]);
+    
     for (int i = 1; i < s.size(); i++)
         s[i - 1] == ' ' ? answer += toupper(s[i]) : answer += tolower(s[i]);
     
