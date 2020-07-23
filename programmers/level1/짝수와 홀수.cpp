f3ecf04c4840e0b12616dfa1cae1bcbd3087efb0
Donghyeon Cho <41141851+hyeon9698@users.noreply.github.com>
2020-07-23 19:51:03 +0900

    Create 짝수와 홀수.cpp

---
+++ programmers/level1/짝수와 홀수.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    
+    return num%2==0 ? "Even" : "Odd";
+}
