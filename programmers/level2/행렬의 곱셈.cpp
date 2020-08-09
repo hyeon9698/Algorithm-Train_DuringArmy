c4eb9a584484f8ce11ac7fec8e099e1aa0804318
Donghyeon Cho <41141851+hyeon9698@users.noreply.github.com>
2020-08-09 09:40:26 +0900

    Update 행렬의 곱셈.cpp

--- programmers/level2/행렬의 곱셈.cpp
+++ programmers/level2/행렬의 곱셈.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 
 using namespace std;
-
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     vector<vector<int>> answer;
     for(int i=0;i<arr1.size();i++){
