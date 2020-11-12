218fc802fb884145309199aa82b1e8d2f34b61e4
Donghyeon Cho <hyeon9698@naver.com>
2020-11-12 19:00:39 +0900

    Update 5397.py

--- baekjoon/5397.py
+++ baekjoon/5397.py
@@ -17,3 +17,29 @@ for _ in range(int(stdin.readline())):
             left.append(typ)
     left.extend(reversed(right))
     print(''.join(left))
+
+
+
+#####################################
+
+test_cases = int(input())
+
+for _ in range(test_cases):
+    left = []
+    right = []
+    pwd = input()
+
+    for x in pwd:
+        if x == ">":
+            if right:
+                left.append(right.pop()) 
+        elif x=="<":
+            if left:
+                right.append(left.pop())
+        elif x=="-":
+            if left:
+                left.pop()
+        else:
+            left.append(x)
+
+  print("".join(left)+"".join(reversed(right)))
