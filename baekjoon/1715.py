d0658f940a1c901b4f8b9eca0a5f1dd5cc13cb85
Donghyeon Cho <hyeon9698@naver.com>
2020-12-07 13:57:09 +0000

    baek

--- baekjoon/1715.py
+++ baekjoon/1715.py
@@ -2,12 +2,10 @@ import heapq
 
 n = int(input())
 data = []
-added_data = []
 result = 0
 for _ in range(n):
     heapq.heappush(data, int(input()))
 while len(data) >= 2:
-    print("heap->", data)
     tmp1 = heapq.heappop(data)
     tmp2 = heapq.heappop(data)
     result = result + tmp1 + tmp2
