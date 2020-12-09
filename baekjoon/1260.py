4ebd25d0f273e450762cc6222b4c808c8f9582b2
Donghyeon Cho <hyeon9698@naver.com>
2020-12-09 19:10:56 +0900

    Update 1260.py

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,9 +1,9 @@
-N,M,V=map(int,input().split())
-matrix=[[0]*(N+1) for i in range(N+1)]
-for i in range(M):
-    a,b = map(int,input().split())
-    matrix[a][b]=matrix[b][a]=1
-visit_list=[0]*(N+1)
+# N,M,V=map(int,input().split())
+# matrix=[[0]*(N+1) for i in range(N+1)]
+#for i in range(M):
+ #   a,b = map(int,input().split())
+ #   matrix[a][b]=matrix[b][a]=1
+#visit_list=[0]*(N+1)
 
 def dfs(V):
     visit_list[V]=1 #방문한 점 1로 표시
@@ -23,6 +23,6 @@ def bfs(V):
                 queue.append(i)
                 visit_list[i]=0
 
-dfs(V)
-print()
-bfs(V)
+#dfs(V)
+#print()
+#bfs(V)
