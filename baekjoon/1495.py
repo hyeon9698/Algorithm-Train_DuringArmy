9013b7910d9ce633b735bd03acea1faa83d14f6b
Donghyeon Cho <hyeon9698@naver.com>
2020-12-12 07:16:52 +0000

    baek

---
+++ baekjoon/1495.py
@@ -0,0 +1,25 @@
+n, s, m = map(int, input().split())
+data = list(map(int, input().split()))
+dp = [[0] * (m+1) for _ in range(n+1)]
+dp[0][s] = True
+for i in range(1, n+1):
+    for j in range(m+1):
+        if dp[i-1][j] == 0:
+            continue
+        if j - data[i-1] >= 0:
+            dp[i][j-data[i-1]] = 1
+        if j + data[i-1] <= m:
+            dp[i][j+data[i-1]] = 1
+        # if dp[i-1][j] is True:
+        #     dp[j] = False
+        #     if 0 <= j + num <= m:
+        #         dp[j+num] = True
+        #     if 0 <= j - num <= m:
+        #         dp[j-num] = True
+        #     print(dp)
+result = -1
+for i in range(m, -1, -1):
+    if dp[n][i] == 1:
+        result = i
+        break
+print(result)
