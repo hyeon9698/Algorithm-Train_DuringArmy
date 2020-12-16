fc25e4122f41a22f5704a53aa580d53335f9bc2f
DonghyeonCho <hyeon9698@naver.com>
2020-12-16 14:18:11 +0000

    baek

--- baekjoon/2012.py
+++ baekjoon/2012.py
@@ -1,10 +1,9 @@
 n = int(input())
 data = []
 result = 0
-org_data = [i for i in range(1, n+1)]
 for _ in range(n):
     data.append(int(input()))
 data.sort()
-for i in range(0, n):
-    result += abs(data[i] - org_data[i])
+for i in range(1, n+1):
+    result += abs(data[i-1] - i)
 print(result)
