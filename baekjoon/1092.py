20420ddee7f0d29e040036b528f39b9eb646fc45
DonghyeonCho <hyeon9698@naver.com>
2020-12-16 14:35:51 +0000

    baek

--- baekjoon/1092.py
+++ baekjoon/1092.py
@@ -4,3 +4,8 @@ limit.sort(reverse=True)
 m = int(input())
 boxes = list(map(int, input().split()))
 boxes.sort(reverse=True)
+for li in limit:
+    for box in boxes:
+        if li >= box:
+            pass
+        elif 
