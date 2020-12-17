0e61129acead71563f9316e37eaba1d1b155f24c
DonghyeonCho <hyeon9698@naver.com>
2020-12-17 13:09:45 +0000

    baek

--- baekjoon/1092.py
+++ baekjoon/1092.py
@@ -1,11 +1,26 @@
+import sys
+
 n = int(input())
 limit = list(map(int, input().split()))
 limit.sort(reverse=True)
 m = int(input())
 boxes = list(map(int, input().split()))
 boxes.sort(reverse=True)
-for li in limit:
-    for box in boxes:
-        if li >= box:
-            pass
-        elif 
+result = 0
+finished = [False] * m
+if boxes[0] > limit[0]:
+    print(-1)
+    sys.exit()
+
+while sum(finished) != m:
+    index = 0
+    for li in limit:
+        while index < m:
+            if li >= boxes[index] and not finished[index]:
+                finished[index] = True
+                index += 1
+                break
+            else:
+                index += 1
+    result += 1
+print(result)
