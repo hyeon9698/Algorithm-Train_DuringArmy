b943063a8ee4a1ca902fb354b0be4ac4edf2aa87
DonghyeonCho <hyeon9698@naver.com>
2020-12-23 13:43:20 +0000

    baek

--- baekjoon/1074.py
+++ baekjoon/1074.py
@@ -1,29 +1,24 @@
-def solve(n, x, y):
-    global result
-    if n == 2:
-        if x == X and y == Y:
-            print(result)
-            return
-        result += 1
-        if x == X and y+1 == Y:
-            print(result)
-            return
-        result += 1
-        if x+1 == X and y == Y:
-            print(result)
-            return
-        result += 1
-        if x+1 == X and y+1 == Y:
-            print(result)
-            return
-        result += 1
-        return
-    solve(n/2, x, y)
-    solve(n/2, x, y+n/2)
-    solve(n/2, x+n/2, y)
-    solve(n/2, x+n/2, y+n/2)
-
-
-result = 0
-N, X, Y = map(int, input().split())
-solve(2 ** N, 0, 0)
+n, r, c = map(int, input().split())
+num = 0
+while n > 0:
+    temp = (2 ** n) // 2
+    if n > 1:
+        if temp > r and temp <= c:
+            num += temp ** 2
+            c -= temp
+        elif temp <= r and temp > c:
+            num += (temp ** 2) * 2
+            r -= temp
+        elif temp <= r and temp <= c:
+            num += (temp ** 2) * 3
+            r -= temp
+            c -= temp
+    elif n == 1:
+        if r == 0 and c == 1:
+            num += 1
+        elif r == 1 and c == 0:
+            num += 2
+        elif r == 1 and c == 1:
+            num += 3
+    n -= 1
+print(num)
