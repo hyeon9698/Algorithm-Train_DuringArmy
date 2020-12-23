ff228bae408f7317cfca202117bd832bbe3bdeb5
DonghyeonCho <hyeon9698@naver.com>
2020-12-23 13:35:20 +0000

    baek

--- baekjoon/1034.py
+++ baekjoon/1034.py
@@ -1,8 +1,19 @@
 n, m = map(int, input().split())
-data = []
-
-x = input().split()
-print(x)
-
-# for _ in range(m):
-#     data.append(input().split())
+arr = [input() for _ in range(n)]
+k = int(input())
+cnt = [0]*n
+if k%2:
+    for i in range(n):
+        zero_cnt = arr[i].count('0')
+        if zero_cnt%2 and zero_cnt <= k:
+            for j in range(n):
+                if arr[i] == arr[j]:
+                    cnt[i] += 1
+else:
+    for i in range(n):
+        zero_cnt = arr[i].count('0')
+        if not zero_cnt%2 and zero_cnt <= k:
+            for j in range(n):
+                if arr[i] == arr[j]:
+                    cnt[i] += 1
+print(max(cnt))
