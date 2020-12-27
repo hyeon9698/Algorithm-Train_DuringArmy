62252f3edbed3dcf2175a386eb246aa88ab6da78
DonghyeonCho <hyeon9698@naver.com>
2020-12-27 00:46:06 +0000

    baek

--- baekjoon/1016.py
+++ baekjoon/1016.py
@@ -1,7 +1,16 @@
 minn, maxn = map(int, input().split())
+eratos = [False for _ in range(maxn-minn+2)]
+i = 2
+cnt = maxn - minn + 1
+while i*i <= maxn:
+    s = minn // i*i
+    if minn % (i*i) != 0:
+        s += 1
+    while s*(i*i) <= maxn:
+        if not eratos[s*(i*i) - minn]:
+            eratos[s*(i*i) - minn] = True
+            cnt -= 1
+        s += 1
+    i += 1
 
-
-def jegop_yield(x):
-    yield x*x
-
-jegop_yieldㅇ
+print(cnt)
