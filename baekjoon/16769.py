902a284efd24acd5fed7485fe20229aa97ac79b7
DonghyeonCho <hyeon9698@naver.com>
2020-12-29 15:02:20 +0000

    baek

---
+++ baekjoon/16769.py
@@ -0,0 +1,17 @@
+import time
+import sys
+# sys.stdin = open('input.txt', 'r')
+C, M = [], []
+for i in range(3):
+    a, b = map(int, input().split())
+    C.append(a)
+    M.append(b)
+for i in range(2):
+    idx = i % 3
+    nxt = (i+1) % 3
+    print('asdf' , M[idx], M[nxt])
+    M[idx] = max(M[idx] - (C[nxt] - M[nxt]), 0)
+    M[nxt] = min(C[nxt], M[nxt] + M[idx])
+    print(M[idx], M[nxt])
+for i in M:
+    print(i)
