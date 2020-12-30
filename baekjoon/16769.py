971c20cd4275240a19e4c650e2ffc2f042fa6d64
DonghyeonCho <hyeon9698@naver.com>
2020-12-30 12:56:49 +0000

    baek

--- baekjoon/16769.py
+++ baekjoon/16769.py
@@ -1,17 +1,16 @@
 import time
 import sys
-# sys.stdin = open('input.txt', 'r')
+sys.stdin = open('input.txt', 'r')
 C, M = [], []
 for i in range(3):
     a, b = map(int, input().split())
     C.append(a)
     M.append(b)
-for i in range(2):
+for i in range(100):
     idx = i % 3
     nxt = (i+1) % 3
-    print('asdf' , M[idx], M[nxt])
-    M[idx] = max(M[idx] - (C[nxt] - M[nxt]), 0)
-    M[nxt] = min(C[nxt], M[nxt] + M[idx])
-    print(M[idx], M[nxt])
+    # print('asdf' , M[idx], M[nxt])
+    M[idx], M[nxt] = max(M[idx] - (C[nxt] - M[nxt]), 0), min(C[nxt], M[nxt] + M[idx])
+    # print(M[idx], M[nxt])
 for i in M:
     print(i)
