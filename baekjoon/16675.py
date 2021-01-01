d28ea7bf8546dd6d5af9650aa7cfa2a4a0a62eaa
DonghyeonCho <hyeon9698@naver.com>
2021-01-01 23:45:37 +0000

    baek

--- baekjoon/16675.py
+++ baekjoon/16675.py
@@ -1,9 +1,9 @@
 import sys
 # sys.stdin = open('input.txt', 'r')
 ML, MR, TL, TR = ('SPR'.index(i) for i in input().split())
-if ML == MR and (ML+2)%3 in (TL, TR):
+if ML == MR and (ML+2)%3 in [TL, TR]:
     print('TK')
-elif TL == TR and (TL+2)%3 in (ML, MR):
+elif TL == TR and (TL+2)%3 in [ML, MR]:
     print('MS')
 else:
     print('?')
