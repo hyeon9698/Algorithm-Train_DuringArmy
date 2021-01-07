2bc2635ae05fb62baadf5ea7ca67a0897b47fde4
Donghyeon Cho <hyeon9698@naver.com>
2021-01-07 14:05:23 +0000

    baek

--- baekjoon/1012.py
+++ baekjoon/1012.py
@@ -1,4 +1,40 @@
-
+from collections import deque
+import sys
+sys.setrecursionlimit(10 ** 4)
+# sys.stdin = open('input.txt', 'r')
+def bfs(x_cor, y_cor):
+        q = deque([(x_cor, y_cor)])
+        while q:
+            x, y = q.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < n and 0 <= ny < m and data[nx][ny] == 1:
+                    q.append((nx, ny))
+                    data[nx][ny] = 0
+def dfs(x_cor, y_cor):
+    x, y = x_cor, y_cor
+    for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < n and 0 <= ny < m and data[nx][ny] == 1:
+                    data[nx][ny] = 0
+                    dfs(nx, ny)
+for _ in range(int(input())):
+    m, n, k = map(int, input().split())
+    data = [[0]*m for _ in range(n)]
+    cnt = 0
+    for _ in range(k):
+        y, x = map(int, input().split())
+        data[x][y] = 1
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 1:
+                dfs(i, j)
+                cnt += 1
+    print(cnt)
 
 
 
