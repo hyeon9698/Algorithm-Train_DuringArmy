4d80e094e0b4431ad9b26ec4a0a646ba3b0565a3
Donghyeon Cho <hyeon9698@naver.com>
2021-01-09 03:49:25 +0000

    baek

--- baekjoon/1759.py
+++ baekjoon/1759.py
@@ -1,18 +1,53 @@
 from itertools import combinations
-
+import sys
+# sys.stdin = open('input.txt', 'r')
 l, c = map(int, input().split())
-data = list(map(str, input().split()))
+data = list(input().split(' '))
 aeiou = ['a', 'e', 'i', 'o', 'u']
 data.sort()
-no_aeiou_count = 0
-flag = False
-for i in list(combinations(data, l)):
-    no_aeiou_count = 0
-    flag = False
-    for j in range(len(i)):
-        if i[j] in aeiou:
-            flag = True
+data_list = list(combinations(data, 4))
+aeiou_count = 0
+not_aeiou_count = 0
+for letter in data_list:
+    for i in letter:
+        if i in aeiou:
+            aeiou_count += 1
         else:
-            no_aeiou_count += 1
-    if no_aeiou_count >= 2 and flag:
-        print(''.join(i))
+            not_aeiou_count += 1
+    if aeiou_count and not_aeiou_count:
+        print(''.join(letter))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from itertools import combinations
+
+# l, c = map(int, input().split())
+# data = list(map(str, input().split()))
+# aeiou = ['a', 'e', 'i', 'o', 'u']
+# data.sort()
+# no_aeiou_count = 0
+# flag = False
+# for i in list(combinations(data, l)):
+#     no_aeiou_count = 0
+#     flag = False
+#     for j in range(len(i)):
+#         if i[j] in aeiou:
+#             flag = True
+#         else:
+#             no_aeiou_count += 1
+#     if no_aeiou_count >= 2 and flag:
+#         print(''.join(i))
