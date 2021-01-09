5d2b2d96fa1b30217c9f27c22cef28e401f3a290
Donghyeon Cho <hyeon9698@naver.com>
2021-01-09 04:32:53 +0000

    baek

--- baekjoon/18404.py
+++ baekjoon/18404.py
@@ -18,9 +18,14 @@ def bfs(x, y):
 #     for i in range(8):
 #         nx = x + dx[i]
 #         ny = y + dy[i]
-#         if 1<=nx<n+1 and 1<=ny<n+1 and count_data[x][y] + 1 < count_data[nx][ny]:
-#             count_data[nx][ny] = count_data[x][y] + 1
-#             dfs(nx, ny)
+#         if 1<=nx<n+1 and 1<=ny<n+1:
+#             if count_data[nx][ny] == 0:
+#                 count_data[nx][ny] = count_data[x][y] + 1
+#                 dfs(nx, ny)
+#             else:
+#                 if count_data[nx][ny] > count_data[x][y]+1:
+#                     count_data[nx][ny] = count_data[x][y] + 1
+#                     dfs(nx, ny)
 
 dx = [-2, -1, 1, 2, 2, 1, -1, -2]
 dy = [1, 2, 2, 1, -1, -2, -2, -1]
@@ -31,7 +36,7 @@ count_data = [[0]*(n+1) for _ in range(n+1)]
 for _ in range(m):
     a, b = map(int, input().split())
     data.append((a, b))
+dfs(x, y)
 # dfs(x, y)
-bfs(x, y)
 for xx, yy in data:
     print(count_data[xx][yy])
