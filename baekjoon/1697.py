16c0e184ee4789443c84d88bdd316175d282de7c
Donghyeon Cho <hyeon9698@naver.com>
2021-01-09 03:41:38 +0000

    baek

--- baekjoon/1697.py
+++ baekjoon/1697.py
@@ -1,18 +1,61 @@
 from collections import deque
-
-
-def bfs(v):
-    q = deque([v])
+import sys
+input = sys.stdin.readline
+def bfs(n):
+    q = deque([n])
+    visited[n] = True
     while q:
         now = q.popleft()
         if now == k:
-            return array[now]
-        for next_val in (now + 1, now - 1, now*2):
-            if 0 <= next_val < 100001 and not array[next_val]:
-                array[next_val] = array[now] + 1
-                q.append(next_val)
-
-
+            return ar[now]
+        for i in (now-1, now+1, now*2):
+            if 0<=i<MAX and 0<=i<MAX and not visited[i]:
+                visited[i] = True
+                ar[i] = ar[now] + 1
+                q.append(i)
 n, k = map(int, input().split())
-array = [0] * (100001)
+MAX = 100001
+ar = [0]*MAX
+visited = [False]*(MAX)
 print(bfs(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import deque
+
+
+# def bfs(v):
+#     q = deque([v])
+#     while q:
+#         now = q.popleft()
+#         if now == k:
+#             return array[now]
+#         for next_val in (now + 1, now - 1, now*2):
+#             if 0 <= next_val < 100001 and not array[next_val]:
+#                 array[next_val] = array[now] + 1
+#                 q.append(next_val)
+
+
+# n, k = map(int, input().split())
+# array = [0] * (100001)
+# print(bfs(n))
--- baekjoon/1987.py
+++ baekjoon/1987.py
@@ -1,24 +1,68 @@
-def bfs(x, y):
+def bfs(a, b):
     global result
     q = set()
-    q.add((x, y, graph[x][y]))
+    q.add((a, b, data[a][b]))
     while q:
-        x, y, step = q.pop()
-        result = max(result, len(step))
+        x, y, letter = q.pop()
+        result = max(result, len(letter))
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if (0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in step):
-                # print(step+graph[nx][ny])
-                q.add((nx, ny, step + graph[nx][ny]))
-
-
+            if 0<=nx<r and 0<=ny<c and data[nx][ny] not in letter:
+                q.add((nx, ny, letter + data[nx][ny]))
 r, c = map(int, input().split())
-graph = []
+data = []
 for i in range(r):
-    graph.append(input())
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+    data.append(input())
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 result = 0
 bfs(0, 0)
 print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def bfs(x, y):
+#     global result
+#     q = set()
+#     q.add((x, y, graph[x][y]))
+#     while q:
+#         x, y, step = q.pop()
+#         result = max(result, len(step))
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if (0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in step):
+#                 # print(step+graph[nx][ny])
+#                 q.add((nx, ny, step + graph[nx][ny]))
+
+
+# r, c = map(int, input().split())
+# graph = []
+# for i in range(r):
+#     graph.append(input())
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+# result = 0
+# bfs(0, 0)
+# print(result)
