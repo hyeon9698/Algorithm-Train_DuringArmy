4a6f780e96550814d582c6b1671bc98bbf7a6002
Donghyeon Cho <hyeon9698@naver.com>
2021-01-13 13:07:19 +0000

    baek

--- baekjoon/14002.py
+++ baekjoon/14002.py
@@ -1,19 +1,23 @@
 import sys
 from copy import deepcopy
-sys.stdin = open('input.txt', 'r')
+# sys.stdin = open('input.txt', 'r')
 n = int(input())
 data = list(map(int, input().split()))
 dp = deepcopy(data)
-maxx = 0
-ans = [[] for _ in range(n)]
-# dp[i]는 i 번째중 제일 큰 수
+rev = [i for i in range(n)]
+idx = 0
 for i in range(n):
-    maxx = 0
-    for j in range(i-1, -1, -1):
-        if data[i] > data[j]:
-            if maxx <= dp[j]:
-                maxx = dp[j]
-                dp[i] = maxx + data[i]
-                ans[i].append(data[j])
-print(ans)
-print(dp)
+    for j in range(i):
+        if data[i] > data[j] and dp[i] < data[i] + dp[j]:
+            dp[i] = data[i] + dp[j]
+            rev[i] = j
+    if dp[idx] < dp[i]:
+        idx = i
+ans = []
+while rev[idx] != idx:
+    ans.append(data[idx])
+    idx = rev[idx]
+ans.append(data[idx])
+ans.sort()
+print(len(ans))
+print(' '.join(map(str, ans)))
