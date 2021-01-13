5f3406f779e9757efcb0f093a4d1e882c221a158
Donghyeon Cho <hyeon9698@naver.com>
2021-01-13 10:03:48 +0000

    baek

--- baekjoon/11055.py
+++ baekjoon/11055.py
@@ -4,14 +4,13 @@ sys.stdin = open('input.txt', 'r')
 n = int(input())
 data = list(map(int, input().split()))
 dp = deepcopy(data)
-maxx = 0
 # dp[i]는 i 번째중 제일 큰 수
 for i in range(n):
-    maxx = 0
-    for j in range(i-1, -1, -1):
+    for j in range(i):
         if data[i] > data[j]:
-            if maxx < dp[j]:
-                maxx = dp[j]
-                dp[i] = maxx + data[i]
-            
+            dp[i] = max(dp[i], data[i] + dp[j])
+        # if data[i] > data[j]:
+        #     if maxx < dp[j]:
+        #         maxx = dp[j]
+        #         dp[i] = maxx + data[i]
 print(max(dp))
---
+++ baekjoon/14002.py
@@ -0,0 +1,19 @@
+import sys
+from copy import deepcopy
+sys.stdin = open('input.txt', 'r')
+n = int(input())
+data = list(map(int, input().split()))
+dp = deepcopy(data)
+maxx = 0
+ans = [[] for _ in range(n)]
+# dp[i]는 i 번째중 제일 큰 수
+for i in range(n):
+    maxx = 0
+    for j in range(i-1, -1, -1):
+        if data[i] > data[j]:
+            if maxx <= dp[j]:
+                maxx = dp[j]
+                dp[i] = maxx + data[i]
+                ans[i].append(data[j])
+print(ans)
+print(dp)
