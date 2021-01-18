42927465c7f3bfe4cdbfee74ae37a19fc46a0a9d
Donghyeon Cho <hyeon9698@naver.com>
2021-01-18 10:28:41 +0000

    baek

--- baekjoon/2014.py
+++ baekjoon/2014.py
@@ -1,17 +1,6 @@
 import sys
 import heapq
 from copy import deepcopy
-# sys.stdin = open('input.txt', 'r')
-# def check(num, data):
-#     while num != 1:
-#         flag = False
-#         for prime_number in data[::-1]:
-#             if num % prime_number == 0:
-#                 num = num / prime_number
-#                 flag = True
-#         if not flag:
-#             break
-#     return flag
 k, n = map(int, input().split())
 data = list(map(int, input().split()))
 lst = deepcopy(data)
@@ -28,13 +17,3 @@ while ith < n:
         if mn * i < 2 ** 32:
             heapq.heappush(lst, mn*i)
 print(mn)
-# cnt = 0
-# ans = []
-# for i in range(min(data), 2 ** 31):
-#     if check(i, data):
-#         cnt += 1
-#         data.append(i)
-#         ans.append(i)
-#     if cnt == n:
-#         break
-# print(ans[-1])
