1f83d2245ed3932d18462cbd858202b8b9ea3621
Donghyeon Cho <hyeon9698@naver.com>
2021-01-19 10:41:08 +0000

    baek

---
+++ baekjoon/2623.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+n, m = map(int, input().split())
+def process(data):
+    
+for _ in range(m):
+    data = list(map(int, input().split()))
+    arr = [[]*(n+1)]
+    for i in range(1, data[0]+1):
+        arr[data[i]]
