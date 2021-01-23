3b9a6b49c6a44c23a0f5a427921bae3a5d44b6d0
Donghyeon Cho <hyeon9698@naver.com>
2021-01-23 00:33:41 +0000

    baek

--- baekjoon/2623.py
+++ baekjoon/2623.py
@@ -1,10 +1,30 @@
 import sys
-sys.stdin = open('input.txt', 'r')
+from collections import deque
+from collections import defaultdict
+# sys.stdin = open('input.txt', 'r')
 n, m = map(int, input().split())
-def process(data):
-    
+ind = [0 for _ in range(n+1)]
+path = defaultdict(list)
 for _ in range(m):
-    data = list(map(int, input().split()))
-    arr = [[]*(n+1)]
-    for i in range(1, data[0]+1):
-        arr[data[i]]
+    singers = list(map(int, input().split()))
+    for i in range(2, singers[0]+1):
+        ind[singers[i]] += 1
+    for i in range(1, singers[0]):
+        path[singers[i]].append(singers[i+1])
+q = deque()
+for i in range(1, n+1):
+    if ind[i] == 0:
+        q.append(i)
+answer = []
+while q:
+    now = q.popleft()
+    answer.append(now)
+    for nxt in path[now]:
+        ind[nxt] -= 1
+        if ind[nxt] == 0:
+            q.append(nxt)
+if len(answer) == n:
+    for i in answer:
+        print(i)
+else:
+    print(0)
