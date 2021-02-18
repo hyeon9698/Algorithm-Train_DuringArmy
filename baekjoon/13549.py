62cb6a61857ef9d093d7c26fd284dad791f1ee2a
Donghyeon Cho <hyeon9698@naver.com>
2021-02-18 11:16:33 +0000

    13549.py

---
+++ baekjoon/13549.py
@@ -0,0 +1,10 @@
+from collections import deque
+import sys
+sys.stdin = open('input.txt', 'r')
+def bfs(n, k):
+    q = deque(n)
+    while q:
+        now = q.popleft()
+        for nxt in ()
+n, k = map(int, input().split())
+bfs(n, k)
