9f777f00fd7565a221bb0278f6916a7b3b6637d4
Donghyeon Cho <hyeon9698@naver.com>
2021-02-22 00:51:55 +0000

    11404_backup.py

--- baekjoon/11404_backup.py
+++ baekjoon/11404_backup.py
@@ -14,5 +14,9 @@ for k in range(1, n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-for i in graph[1:]:
-    print(' '.join(map(str, i[1:])))
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == inf:
+            graph[i][j] = 0
+        print(graph[i][j], end=' ')
+    print()
