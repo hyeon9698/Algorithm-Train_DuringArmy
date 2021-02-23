f2002780720555eb6d122c70e3eaf6c2a0e1cecf
Donghyeon Cho <hyeon9698@naver.com>
2021-02-23 14:00:20 +0000

    1916.py

--- baekjoon/1916.py
+++ baekjoon/1916.py
@@ -1,19 +1,18 @@
 import sys
 from collections import deque
+from heapq import *
 sys.stdin = open('input.txt', 'r')
-def kruskal(start, end):
+def dijkstra(start, end):
     visited[start] = 0
-    q = deque([(start, 0)])
+    q = [(start, 0)]
+    heapify(q)
     while q:
-        now, now_cost = q.popleft()
-        # print(now, now_cost, visited)
-        # if now == end:
-            # return visited[now]
+        now, now_cost = heappop(q)
         for nxt, nxt_cost in data[now]:
             cost = now_cost + nxt_cost
             if visited[nxt] > cost:
                 visited[nxt] = cost
-                q.append((nxt, cost))
+                heappush(q, (nxt, cost))
 n = int(input())
 m = int(input())
 data = [[] for _ in range(n+1)]
@@ -22,5 +21,5 @@ for _ in range(m):
     a, b, c = map(int, input().split())
     data[a].append((b, c))
 start, end = map(int, input().split())
-kruskal(start, end)
+dijkstra(start, end)
 print(visited[end])
