20ed05a284cb9e8799f4ed0e5d0c63bae700b3c0
Donghyeon Cho <hyeon9698@naver.com>
2021-04-03 01:14:40 +0900

    1238.py

--- baekjoon/1238.py
+++ baekjoon/1238.py
@@ -1,2 +1,8 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+n, m, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    start, end, cost = map(int, input().split())
+    graph[start].append((end, cost))
+print(graph)
