278445b078cb20b7b762c0f9ac82eec95acfe875
Donghyeon Cho <hyeon9698@naver.com>
2021-04-09 15:45:01 +0900

    15652.py

--- baekjoon/15652.py
+++ baekjoon/15652.py
@@ -1,27 +1,40 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+# #include<iostream>
+# #include<vector>
+# using namespace std;
  
-void dfs(int N, int M, vector<int>& vt){
-    if(vt.size() == M){
-        for(int i: vt)
-            cout<<i<<" ";
-        cout<<"\n";
-        return;
-    }
+# void dfs(int N, int M, vector<int>& vt){
+#     if(vt.size() == M){
+#         for(int i: vt)
+#             cout<<i<<" ";
+#         cout<<"\n";
+#         return;
+#     }
  
-    for(int n=1; n<=N; n++){
-        if(!vt.empty() && vt.back() > n) continue;
+#     for(int n=1; n<=N; n++){
+#         if(!vt.empty() && vt.back() > n) continue;
  
-        vt.push_back(n);
-        dfs(N,M,vt);
-        vt.pop_back();
-    }
-}
-int main(){
-    int N,M;
-    vector<int> vt;
-    cin>>N>>M;
-    dfs(N,M,vt);
-    return 0;
-}
+#         vt.push_back(n);
+#         dfs(N,M,vt);
+#         vt.pop_back();
+#     }
+# }
+# int main(){
+#     int N,M;
+#     vector<int> vt;
+#     cin>>N>>M;
+#     dfs(N,M,vt);
+#     return 0;
+# }
+def dfs(n,m,_list):
+    if len(_list) == m:
+        print(' '.join(map(str,_list)))
+        return
+    for i in range(1, n+1):
+        if len(_list) != 0 and _list[-1] > i:
+            continue
+        _list.append(i)
+        dfs(n,m,_list)
+        _list.pop()
+n, m = map(int, input().split())
+_list = []
+dfs(n,m,_list)
