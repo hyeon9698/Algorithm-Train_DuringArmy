7c535d803997f979e6616bce22a6fa13c3440c41
Donghyeon Cho <hyeon9698@naver.com>
2021-07-02 11:21:15 +0900

    [update]

--- baekjoon/test.py
+++ baekjoon/test.py
@@ -1,92 +1,37 @@
-import heapq
 import sys
-# sys.stdin = open('input.txt', 'r')
+from copy import deepcopy
+from collections import deque
+sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
-def dijkstra(start, data, distance):
-    heap_data = []
-    heapq.heappush(heap_data, (0, start))
-    distance[start] = 0
-    while heap_data:
-        dist, now = heapq.heappop(heap_data)
-        if distance[now] < dist:
+def bfs(i, j):
+    visited[i][j] = True
+    q = deque([(i, j)])
+    cnt = 1
+    while q:
+        x, y = q.popleft()
+        for k in range(4):
+            xx = x + dx[k]
+            yy = y + dy[k]
+            if 0<=xx<n and 0<=yy<m and not visited[xx][yy] and data[xx][yy] == 0:
+                visited[xx][yy] = True
+                cnt += 1
+                q.append((xx, yy))
+    ans[i][j] = cnt
+def dfs():
+    pass
+n, m = map(int, input().split())
+data = [list(map(int, list(input()))) for _ in range(n)]
+visited_false = [[False for _ in range(m)] for _ in range(n)]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+ans = [[0 for _ in range(m)] for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        if data[i][j] == 0:
             continue
-        for nextt, time in data[now]:
-            cost = time + dist
-            if cost < distance[nextt]:
-                distance[nextt] = cost
-                heapq.heappush(heap_data, (cost, nextt))
-
-def process():
-    n, d, c = map(int, input().split())
-    data = [[] for _ in range(n+1)]
-    for _ in range(d):
-        a, b, t = map(int, input().split())
-        data[b].append((a, t))
-    distance = [1e9]*(n+1)
-    dijkstra(c, data, distance)
-    count = 0
-    m = 0
-    for d in distance:
-        if d != 1e9:
-            count += 1
-            m = max(m, d)
-    print(count, m)
-for _ in range(int(input())):
-    process()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import heapq
-# import sys
-# input = sys.stdin.readline
-
-
-# def dijkstra(start):
-#     heap_data = []
-#     heapq.heappush(heap_data, (0, start))
-#     distance[start] = 0
-#     while heap_data:
-#         dist, now = heapq.heappop(heap_data)
-#         if distance[now] < dist:
-#             continue
-#         for i in adj[now]:
-#             cost = dist + i[1]
-#             if distance[i[0]] > cost:
-#                 distance[i[0]] = cost
-#                 heapq.heappush(heap_data, (cost, i[0]))
-
-
-# for _ in range(int(input())):
-#     n, m, start = map(int, input().split())
-#     adj = [[] for i in range(n+1)]
-#     distance = [1e9] * (n+1)
-#     for _ in range(m):
-#         x, y, cost = map(int, input().split())
-#         adj[y].append((x, cost))
-#     dijkstra(start)
-#     count = 0
-#     max_distance = 0
-#     for i in distance:
-#         if i != 1e9:
-#             count += 1
-#             if i > max_distance:
-#                 max_distance = i
-#     print(count, max_distance)
+        visited = deepcopy(visited_false)
+        data[i][j] = 0
+        bfs(i, j)
+        data[i][j] = 1
+for i in ans:
+    print(''.join(map(str,i)))
